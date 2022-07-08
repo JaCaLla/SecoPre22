a76cd8b9d5e50f57908f16a4a9aaca38b7fd7ec4
Javier Calatrava <javi.calatrava@gmail.com>
2022-07-08 12:06:24 +0200

    Empty XCode project

---
+++ SecoPre22/SecoPre22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecoPre22
+//
+//  Created by Javier Calatrava on 8/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
